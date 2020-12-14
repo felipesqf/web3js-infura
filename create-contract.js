b58fd79e673add8baa605ed78963bd2c5220a008
Felipe Ferreira <felipesqf@gmail.com>
2020-12-15 00:10:49 +1100

    events

--- create-contract.js
+++ create-contract.js
@@ -1,11 +1,31 @@
 let Web3 = require("web3");
-let url_infura = "https://mainnet.infura.io/v3/489fa9bf01b84e339374b0d440d0be6d";
-let web3 = new Web3(url_infura);
+let ws = "wss://mainnet.infura.io/ws/v3/489fa9bf01b84e339374b0d440d0be6d";
+let web3 = new Web3(ws);
 
 const link_address = '0x514910771AF9Ca656af840dff83E8264EcF986CA';
 const link_abi = [{"constant":true,"inputs":[],"name":"name","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_spender","type":"address"},{"name":"_value","type":"uint256"}],"name":"approve","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_from","type":"address"},{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transferFrom","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"decimals","outputs":[{"name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_value","type":"uint256"},{"name":"_data","type":"bytes"}],"name":"transferAndCall","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"_spender","type":"address"},{"name":"_subtractedValue","type":"uint256"}],"name":"decreaseApproval","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"_owner","type":"address"}],"name":"balanceOf","outputs":[{"name":"balance","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"symbol","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transfer","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"_spender","type":"address"},{"name":"_addedValue","type":"uint256"}],"name":"increaseApproval","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"_owner","type":"address"},{"name":"_spender","type":"address"}],"name":"allowance","outputs":[{"name":"remaining","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"name":"from","type":"address"},{"indexed":true,"name":"to","type":"address"},{"indexed":false,"name":"value","type":"uint256"},{"indexed":false,"name":"data","type":"bytes"}],"name":"Transfer","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"owner","type":"address"},{"indexed":true,"name":"spender","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Approval","type":"event"}]
 
 const link_contract = new web3.eth.Contract(link_abi, link_address);
 
-console.log(link_contract);
+//get the name of the token
+link_contract.methods.name().call((err, res) =>{
+    if(!err){
+        console.log(res)
+    }
+})
+//get the total supply
+link_contract.methods.totalSupply().call((err, res) =>{
+        console.log(res)
+})
 
+link_contract.methods.symbol().call((err, res) =>{
+    console.log(res)
+})
+//getting the lastest event on chainlink contract ///
+link_contract.events.allEvents({fromBlock: 'latest'}, (err, res) =>{
+    console.log(res)
+})
+//getting the latest transfer
+link_contract.events.Transfer({fromBlock: 'latest'}, (err, res) =>{
+    console.log(res)
+})
--- subs-log.js
+++ subs-log.js
@@ -24,3 +24,4 @@ web3.eth.subscribe('logs', { address: "0x514910771af9ca656af840dff83e8264ecf986c
         console.log(err)
     }
 })
+
